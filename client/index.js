0c49f549aa2b56fed1f536598bc5ea0a0bd45f14
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-08-05 21:00:23 +0530

    update canvas background color and add snake drawing function

--- client/index.js
+++ client/index.js
@@ -1,19 +1,133 @@
-const socket = io('ws://localhost:5000')
+const socket = io('ws://localhost:5000');
 
 socket.on('info', (msg) => {
-  console.log(msg)
-})
+  console.log(msg);
+});
 
-socket.emit('info', 'hello from client')
+socket.emit('info', 'hello from client');
 
-const canvasSize = 600
-const blockSize = canvasSize / 10
+const path = {
+  1: { x: 0, y: 9 },
+  2: { x: 1, y: 9 },
+  3: { x: 2, y: 9 },
+  4: { x: 3, y: 9 },
+  5: { x: 4, y: 9 },
+  6: { x: 5, y: 9 },
+  7: { x: 6, y: 9 },
+  8: { x: 7, y: 9 },
+  9: { x: 8, y: 9 },
+  10: { x: 9, y: 9 },
+  11: { x: 9, y: 8 },
+  12: { x: 8, y: 8 },
+  13: { x: 7, y: 8 },
+  14: { x: 6, y: 8 },
+  15: { x: 5, y: 8 },
+  16: { x: 4, y: 8 },
+  17: { x: 3, y: 8 },
+  18: { x: 2, y: 8 },
+  19: { x: 1, y: 8 },
+  20: { x: 0, y: 8 },
+  21: { x: 0, y: 7 },
+  22: { x: 1, y: 7 },
+  23: { x: 2, y: 7 },
+  24: { x: 3, y: 7 },
+  25: { x: 4, y: 7 },
+  26: { x: 5, y: 7 },
+  27: { x: 6, y: 7 },
+  28: { x: 7, y: 7 },
+  29: { x: 8, y: 7 },
+  30: { x: 9, y: 7 },
+  31: { x: 9, y: 6 },
+  32: { x: 8, y: 6 },
+  33: { x: 7, y: 6 },
+  34: { x: 6, y: 6 },
+  35: { x: 5, y: 6 },
+  36: { x: 4, y: 6 },
+  37: { x: 3, y: 6 },
+  38: { x: 2, y: 6 },
+  39: { x: 1, y: 6 },
+  40: { x: 0, y: 6 },
+  41: { x: 0, y: 5 },
+  42: { x: 1, y: 5 },
+  43: { x: 2, y: 5 },
+  44: { x: 3, y: 5 },
+  45: { x: 4, y: 5 },
+  46: { x: 5, y: 5 },
+  47: { x: 6, y: 5 },
+  48: { x: 7, y: 5 },
+  49: { x: 8, y: 5 },
+  50: { x: 9, y: 5 },
+  51: { x: 9, y: 4 },
+  52: { x: 8, y: 4 },
+  53: { x: 7, y: 4 },
+  54: { x: 6, y: 4 },
+  55: { x: 5, y: 4 },
+  56: { x: 4, y: 4 },
+  57: { x: 3, y: 4 },
+  58: { x: 2, y: 4 },
+  59: { x: 1, y: 4 },
+  60: { x: 0, y: 4 },
+  61: { x: 0, y: 3 },
+  62: { x: 1, y: 3 },
+  63: { x: 2, y: 3 },
+  64: { x: 3, y: 3 },
+  65: { x: 4, y: 3 },
+  66: { x: 5, y: 3 },
+  67: { x: 6, y: 3 },
+  68: { x: 7, y: 3 },
+  69: { x: 8, y: 3 },
+  70: { x: 9, y: 3 },
+  71: { x: 9, y: 2 },
+  72: { x: 8, y: 2 },
+  73: { x: 7, y: 2 },
+  74: { x: 6, y: 2 },
+  75: { x: 5, y: 2 },
+  76: { x: 4, y: 2 },
+  77: { x: 3, y: 2 },
+  78: { x: 2, y: 2 },
+  79: { x: 1, y: 2 },
+  80: { x: 0, y: 2 },
+  81: { x: 0, y: 1 },
+  82: { x: 1, y: 1 },
+  83: { x: 2, y: 1 },
+  84: { x: 3, y: 1 },
+  85: { x: 4, y: 1 },
+  86: { x: 5, y: 1 },
+  87: { x: 6, y: 1 },
+  88: { x: 7, y: 1 },
+  89: { x: 8, y: 1 },
+  90: { x: 9, y: 1 },
+  91: { x: 9, y: 0 },
+  92: { x: 8, y: 0 },
+  93: { x: 7, y: 0 },
+  94: { x: 6, y: 0 },
+  95: { x: 5, y: 0 },
+  96: { x: 4, y: 0 },
+  97: { x: 3, y: 0 },
+  98: { x: 2, y: 0 },
+  99: { x: 1, y: 0 },
+  100: { x: 0, y: 0 },
+};
 
-const canvasEle = document.getElementById("canvas")
-canvasEle.height = canvasSize
-canvasEle.width = canvasSize
-canvasEle.style.backgroundColor = "#aa0"
-const ctx = canvasEle.getContext("2d")
+const snake = [
+  { m: 99, t: 5 },
+  { m: 93, t: 5 },
+  { m: 95, t: 10 },
+];
+
+const ladder = [
+  { m: 6, t: 90 },
+  { m: 6, t: 12 },
+];
+
+const canvasSize = 600;
+const blockSize = canvasSize / 10;
+
+const canvasEle = document.getElementById('canvas');
+canvasEle.height = canvasSize;
+canvasEle.width = canvasSize;
+canvasEle.style.backgroundColor = '#868';
+const ctx = canvasEle.getContext('2d');
 
 const drawCircle = (x, y, r, fillColor) => {
   ctx.beginPath();
@@ -21,9 +135,9 @@ const drawCircle = (x, y, r, fillColor) => {
   ctx.fillStyle = fillColor;
   ctx.fill();
   ctx.lineWidth = 2;
-  ctx.strokeStyle = "blue";
+  ctx.strokeStyle = 'blue';
   ctx.stroke();
-}
+};
 
 const drawLine = (x1, y1, x2, y2) => {
   ctx.beginPath();
@@ -31,16 +145,38 @@ const drawLine = (x1, y1, x2, y2) => {
   ctx.lineTo(x2, y2);
   ctx.lineWidth = 1;
   ctx.stroke();
-}
+};
 
+const drawPawn = (x, y, color) => {
+  drawCircle(
+    blockSize / 2 + blockSize * x,
+    blockSize / 2 + blockSize * y,
+    blockSize / 2 - blockSize / 6,
+    color
+  );
+};
+
+// y axis line draw
 for (let i = 1; i < 10; i++) {
-  drawLine(blockSize * i, 0, blockSize * i, canvasSize)
+  drawLine(blockSize * i, 0, blockSize * i, canvasSize);
 }
 for (let i = 1; i < 10; i++) {
-  drawLine(0, blockSize * i, canvasSize, blockSize * i)
+  drawLine(0, blockSize * i, canvasSize, blockSize * i);
 }
 
+// drawPawn(1, 1, "green")
+// drawPawn(3, 0, "red")
 
+const drawSnake = () => {
+  ctx.beginPath();
+  ctx.lineWidth = '2';
+  ctx.fillStyle = '#dd7';
+  ctx.strokeStyle = '#fcf';
+  ctx.rect(50, 100, 500, 200);
+  ctx.fill();
+  ctx.stroke();
+};
 
+drawSnake();
 
-drawCircle(blockSize / 2 + blockSize * 2, blockSize / 2 + blockSize * 1, blockSize / 2 - blockSize / 5, "red")
+// console.log(path)
